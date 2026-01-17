fdda30a29d3422e92e257126b92a7a06f3e3a8f6
imharryzhu <imharryzhu@gmail.com>
2026-01-17 22:54:09 +0800

    build mac app is ok!

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MyGreateWindow
 A greate gui application for myself.
+
+# build my app
+
+```shell
+uv run python tools/build.py
+```
---
+++ tools/build.py
@@ -0,0 +1,27 @@
+
+import sys
+import os
+import subprocess
+from pathlib import Path
+
+script_path = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(script_path)
+
+def build_mac_app():
+    print("Building macOS application...")
+
+    cmd = [
+        sys.executable, 
+        '-m', 'nuitka',
+        '--mode=app',
+        '--enable-plugin=pyside6',
+        f'{project_root}/src/main.py',
+        f'--output-dir={project_root}/build_mac_app',
+    ]
+    subprocess.run(cmd)
+
+
+if __name__ == "__main__":
+    print(project_root  )
+    if sys.platform == "darwin":
+        build_mac_app()
