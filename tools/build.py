3785b14ad856b3332fda1cfe29351cb544d6c661
hui.zhu@mihoyo.com <hui.zhu@mihoyo.com>
2026-01-19 11:13:44 +0800

    build win app is ok!

--- tools/build.py
+++ tools/build.py
@@ -20,8 +20,24 @@ def build_mac_app():
     ]
     subprocess.run(cmd)
 
+def build_win_app():
+    print("Building Windows application...")
+
+    cmd = [
+        sys.executable, 
+        '-m', 'nuitka',
+        '--standalone',
+        '--windows-console-mode=disable',
+        '--enable-plugin=pyside6',
+        f'{project_root}/src/main.py',
+        f'--output-dir={project_root}/build_win_app',
+    ]
+    subprocess.run(cmd)
+
 
 if __name__ == "__main__":
     print(project_root  )
     if sys.platform == "darwin":
         build_mac_app()
+    if sys.platform == "win32":
+        build_win_app()
